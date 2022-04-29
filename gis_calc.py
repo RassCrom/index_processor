622cda2d31782e51b1b578961a720135d2f9e678
RassCrom <rkoenigseggagera@gmail.com>
2022-04-29 15:12:02 +0600

    Write comments

--- gis_calc.py
+++ gis_calc.py
@@ -1,21 +1,13 @@
 import rasterio
-from rasterio import plot
-import matplotlib.pyplot as plt
 from osgeo import gdal, ogr
 import numpy as np
 import os
-import sys
 import geopandas as gp
-from rasterio.plot import show
-import osgeo.osr as osr
 
-# Function to get info from html's forms
+# Function
 def get_ndwi(green, nir, res):
 
     dst_crs = 'EPSG:4326'
-    # To specife the main folders
-    # a = os.listdir(r"C:/Users/rkoen/OneDrive/Документы/Cartometria/clipped" + r"/green_one")
-    # b = os.listdir(r"C:/Users/rkoen/OneDrive/Документы/Cartometria/clipped" + r"/nir_one")
     path1 = green + "/"
     path2 = nir + "/"
     a = os.listdir(path1)
@@ -24,21 +16,20 @@ def get_ndwi(green, nir, res):
     for i, y in zip(a, b):
 
         # Reading images
-        # band3 = rasterio.open(path1 + i)
-        # band5 = rasterio.open(path2 + y)
         ds_band3 = gdal.Open(path1 + i)
         ds_band5 = gdal.Open(path2 + y)
         
-        dsReprj_3 = gdal.Warp(res + '/reprjct_' + i[13:27] + '_' + i[-6:], ds_band3, dstSRS="EPSG:4326")
+        # Reprojecting images for web based projection
+        dsReprj_3 = gdal.Warp(res + '/reprjct_' + i[13:27] + '_' + i[-6:], ds_band3, dstSRS=dst_crs)
         name_3 = res + '/reprjct_' + i[13:27] + '_' + i[-6:]
-        dsReprj_5 = gdal.Warp(res + '/reprjct_' + y[13:27] + '_' + y[-6:], ds_band5, dstSRS='EPSG:4326')
+        dsReprj_5 = gdal.Warp(res + '/reprjct_' + y[13:27] + '_' + y[-6:], ds_band5, dstSRS=dst_crs)
         name_5 = res + '/reprjct_' + y[13:27] + '_' + y[-6:]
         
+        # Re-open images in rasterio
         band3 = rasterio.open(name_3)
         band5 = rasterio.open(name_5)
-        
-        # band3 = rasterio.open(r"C:/Users/rkoen/OneDrive/Документы/Cartometria/clipped/green_one/" + i)
-        # band5 = rasterio.open(r"C:/Users/rkoen/OneDrive/Документы/Cartometria/clipped/nir_one/" + y)
+
+        # Getting first band of images
         green = band3.read(1).astype('float64')
         nir = band5.read(1).astype('float64')
 
@@ -61,12 +52,14 @@ def get_ndwi(green, nir, res):
         ndwiImage.write(ndwi,1)
         ndwiImage.close()
         
-        #Classyfing Index (Selecting only water objects >0.4)
+        # Classyfing Index (Selecting only water objects >0.2)
         raster = rasterio.open(res + '/ndwi_' + i)
         raster_r = raster.read()
         lista = raster_r.copy()
         lista[np.where(lista >= 0.2)] = 1
         lista[np.where(lista < 0.2)] = 0
+
+        # Saving index
         with rasterio.open(res + '/ndwi_' + i + '_class' + '.TIF', 'w',
                            driver=raster.driver,
                            height=raster.height,
@@ -80,9 +73,7 @@ def get_ndwi(green, nir, res):
         
         #Vectorize image
         b = gdal.Open(res + '/ndwi_' + i + '_class' + '.TIF')
-        b.RasterCount
         d = b.GetRasterBand(1) 
-        print(b.RasterCount)
         drv = ogr.GetDriverByName('ESRI Shapefile')
         outfile = drv.CreateDataSource(res + '/ndwi_' + i + '_class' + '.shp') 
         outlayer = outfile.CreateLayer('polygonized raster', srs = None )
@@ -93,14 +84,15 @@ def get_ndwi(green, nir, res):
         
         #Querying only vector water objects
         vec_crs = gp.read_file(res + '/ndwi_' + i + '_class' + '.shp')
-        vec_crs = vec_crs.set_crs("EPSG:3857")
+        vec_crs = vec_crs.set_crs(dst_crs)
         vec3 = vec_crs[vec_crs["Water"].isin([1])]
-        vec3 = vec3.set_crs("EPSG:3857")
+        vec3 = vec3.set_crs(dst_crs)
         vec3.to_file(res + '/ndwi_' + i + '_class_selected.shp')   
         gdf = gp.read_file(res + '/ndwi_' + i + '_class_selected.shp')
         gdf_lat_lng = gdf.to_crs(4326)
         gdf_save = gdf_lat_lng.to_file(res + '/ndwi_' + i + '_class_selected_latlng.geojson', driver='GeoJSON')
-        
+
+get_ndwi('D:/diplom/data/green', 'D:/diplom/data/nir', 'D:/diplom/data_process')
                            
 
         
